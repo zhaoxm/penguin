eff59ed5d8aa08fa96d29eb293f1215a3cde523d
zhaoxm <hizhaoxm@gmail.com>
2024-08-16 17:23:42 +0800

    add auth&user module

--- apps/cms-server/src/app.module.ts
+++ apps/cms-server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
 })
 export class AppModule {}
