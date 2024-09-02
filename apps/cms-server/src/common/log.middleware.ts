da1480525bea631d01e46060e15e1aaf0e77700a
zhaoxm <hizhaoxm@gmail.com>
2024-09-02 08:52:41 +0800

    fix

--- apps/cms-server/src/common/log.middleware.ts
+++ apps/cms-server/src/common/log.middleware.ts
@@ -3,7 +3,6 @@ import { NextFunction, Request, Response } from 'express';
 
 export class LogMiddleWare implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(new Date().toISOString());
     console.time('useTime');
     next();
     console.timeEnd('useTime');
