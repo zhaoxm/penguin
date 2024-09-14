46d5845f00f363b1ff27b576a0f8de4a3eedac73
zhaoxm <hizhaoxm@gmail.com>
2024-09-14 16:46:53 +0800

    add

---
+++ packages/form-builder/src/index.ts
@@ -0,0 +1,3 @@
+import content from "./test.txt";
+
+console.log(content);
--- packages/form-builder/webpack.config.ts
+++ packages/form-builder/webpack.config.ts
@@ -1,7 +1,20 @@
 import { Configuration } from "webpack";
 
 const config: Configuration = {
-  entry: "",
+  entry: "./src/index.ts",
+  mode: "development",
+  output: {
+    filename: "my-first-webpack.bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: "raw-loader" },
+      {
+        test: /\.ts(x)$/,
+        use: "ts-loader",
+      },
+    ],
+  },
 };
 
 export default config;
